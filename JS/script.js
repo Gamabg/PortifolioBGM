5d13c495e6ea323eb8829d933afd5676bf6a5757
Bruno Gama <106169955+brunogama18@users.noreply.github.com>
2025-05-25 18:19:49 -0300

    Modificações

--- JS/script.js
+++ JS/script.js
@@ -1,1 +1,7 @@
+const hamburger = document.querySelector('.hamburger');
+const headerMenu = document.querySelector('.header-menu');
+
+hamburger.addEventListener('click', () => {
+    headerMenu.classList.toggle('active');
+});
 
